8885b1eec797a89c14c74a832ccd6a7bd41b2450
Dominique <dom.ulysse.gross@gmail.com>
2021-12-01 10:09:51 +0100

    End of Day 1

--- Day_1/main.jl
+++ Day_1/main.jl
@@ -1,9 +1,21 @@
 #### DAY 1 ####
-
+using Statistics
 # Part 1
 
-input1 = collect.(readlines("Day_1/part1.txt"))
+input1 = parse.(Int,collect(readlines("Day_1/part1.txt")))
+
+function part1(input)
+    return sum([ x>0 ? 1 : 0 for x in diff(input) ])
+end
+
+part1(input1)
 
 # Part 2
 
-input2 = collect.(readlines("Day_1/part2.txt"))
+input2 = parse.(Int,collect(readlines("Day_1/part1.txt")))
+
+function part2(input)
+   part1([mean(input[x:x+2]) for x in 1:length(input)-2])
+end
+
+part2(input2)
